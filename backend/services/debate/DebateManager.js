db326bb5a7239c23099772bd3563d7c1442c9970
WreckItBoss <isseymatsumoto0911@gmail.com>
2026-06-22 13:29:09 +0900

    [Documentation] #20 Making Debate Manager class to orchestrate the overall debate

---
+++ backend/services/debate/DebateManager.js
@@ -0,0 +1,12 @@
+class DebateManager{
+    constructor(){
+        this.agents = [];
+        this.debateHistory = [];
+    }
+
+    async initializeAgents(){}
+    async initializeOpinions(){}
+    async runRound(){}
+    async runDebate(){}
+
+}
